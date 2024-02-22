3c880384d109bfb91af3b76bfd2bd89382e55389
Esdras Copado <copadoe6@gmail.com>
2024-02-22 01:30:59 -0700

    actualizacion cors

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const client = new MercadoPagoConfig({ accessToken: "TEST-6954976907151707-02192
 const app = express();
 const port = 3000;
 
+app.disable('x-powered-by')
+
 app.use(cors());
 app.use(express.json());
 
@@ -15,36 +17,40 @@ app.get("/", (req, res) => {
   res.send("¡Bienvenido!");
 });
 
+const ACCEPTED_ORIGINS = ['http://localhost:3000','https://botasjusaino.netlify.app'];
+
 app.post("/create_preference", async (req, res) => {
-    try {
-      const body = {
-        items: [
-          {
-            title: req.body.title,
-            quantity: Number(req.body.quantity),
-            unit_price: Number(req.body.price), // Corregido: "unit_price" en lugar de "unid_price"
-            currency_id: "MXN",
-          },
-        ],
-        back_urls: {
-          success: "https://botasjusaino.netlify.app/", // Corregido: "success" en lugar de "succes"
-          failure: "https://botasjusaino.netlify.app/e-shop.html",
-          pending: "https://botasjusaino.netlify.app/e-shop.html",
+  const origin = req.header('origin');
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  try {
+    const body = {
+      items: [
+        {
+          title: req.body.title,
+          quantity: Number(req.body.quantity),
+          unit_price: Number(req.body.price),
+          currency_id: "MXN",
         },
-        auto_return: "approved"
-      };
-      
-      // Crea la preferencia de pago utilizando la clase Preference
-      const preference = new Preference(client); // Reemplaza 'client' con tu instancia de MercadoPago
-      const result = await preference.create({ body });
-      
-      // Devuelve el ID de la preferencia creada
-      res.json({ id: result.id });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Error al crear la preferencia :(" });
-    }
-  });
+      ],
+      back_urls: {
+        success: "https://botasjusaino.netlify.app/",
+        failure: "https://botasjusaino.netlify.app/e-shop.html",
+        pending: "https://botasjusaino.netlify.app/e-shop.html",
+      },
+      auto_return: "approved"
+    };
+    
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
+    
+    res.json({ id: result.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al crear la preferencia :(" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`El servidor está corriendo en el puerto ${port}`);
